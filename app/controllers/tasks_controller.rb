52edc18ab7f96beee857f6afd5f9ee67ef08e154
hitohiro.nagahashi <vitz1300cat@gmail.com>
2020-05-09 15:09:42 +0000

    課題提出

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,10 @@
 class TasksController < ApplicationController
+  before_action :set_task, only:[:show, :edit, :update, :destroy]
   def index
       @task=Task.all
   end
 
   def show
-      @task=Task.find(params[:id])
   end
 
   def new
@@ -23,12 +23,9 @@ class TasksController < ApplicationController
       end
   end
   def edit
-      @task=Task.find(params[:id])
   end
 
   def update
-    @task=Task.find(params[:id])
-    
     if @task.update(task_params)
       flash[:success] = 'タスクが正常に投稿されました'
       redirect_to @task
@@ -38,7 +35,6 @@ class TasksController < ApplicationController
     end
   end
   def destroy
-    @task=Task.find(params[:id])
     @task.destroy
     
     flash[:success]='タスクは正常に削除されました'
@@ -46,8 +42,10 @@ class TasksController < ApplicationController
   end
   
   private
-  
+  def set_task
+    @task = Task.find(params[:id])
+  end
   def task_params
-      params.require(:task).permit(:content)
+      params.require(:task).permit(:content, :status)
   end
 end
