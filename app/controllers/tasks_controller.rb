68d2ab2744b320c24d7df6c57f77799f84f49bc5
hitohiro.nagahashi <vitz1300cat@gmail.com>
2020-05-23 15:28:24 +0000

    Lesson15tasklist

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,8 @@
 class TasksController < ApplicationController
+  before_action :require_user_logged_in
+   before_action :correct_user, only: [:destroy, :update]
   before_action :set_task, only:[:show, :edit, :update, :destroy]
+
   def index
       @task=Task.all
   end
@@ -8,11 +11,15 @@ class TasksController < ApplicationController
   end
 
   def new
+    if logged_in?
+      @task = current_user.tasks.build
+    end
       @task=Task.new
   end
 
   def create
-      @task=Task.new(task_params)
+      
+      @task=current_user.tasks.build(task_params)
       
       if @task.save
         flash[:success] = 'タスクが正常に投稿されました'
@@ -47,5 +54,11 @@ class TasksController < ApplicationController
   end
   def task_params
       params.require(:task).permit(:content, :status)
+  end
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,19 @@
 Rails.application.routes.draw do
-    root to: 'tasks#index'
-    resources :tasks
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # get 'sessions/new'
+  # get 'sessions/create'
+  # get 'sessions/destroy'
+  # get 'users/index'
+  # get 'users/show'
+  # get 'users/new'
+  # get 'users/create'
+  # ログイン
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+ # サインアップ
+  get 'signup', to: 'users#new'
+  resources :users, only: [:new, :create]
+  # タスク管理
+  root to: 'tasks#index'
+  resources :tasks
 end
