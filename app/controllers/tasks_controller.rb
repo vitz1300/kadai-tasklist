1a47b623de2b687a313c3b5e1c714620c6803fba
hitohiro.nagahashi <vitz1300cat@gmail.com>
2020-05-27 09:42:02 +0000

    課題再提出2020/5/27午後

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,7 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
   before_action :correct_user, only: [:destroy, :update, :show, :edit]
-  before_action :set_task, only:[:show, :edit, :update, :destroy]
+  
 
   def index
       @task=current_user.tasks
@@ -49,9 +49,6 @@ class TasksController < ApplicationController
   end
   
   private
-  def set_task
-    @task = Task.find(params[:id])
-  end
   def task_params
       params.require(:task).permit(:content, :status)
   end
